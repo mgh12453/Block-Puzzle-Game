c9c55c5512951eedd76eaf2a9863d5a4eb029c9a
MohammadReza <mgh12453@gmail.com>
2023-01-22 14:04:29 +0330

    Fix Bug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+class game
+{
+private:
+	vector<vector<char>> buff;
+	int size;
+public:
+	game(int init_size = 11){
+		size = init_size;
+		for(int i = 0; i < size; i ++)
+			buff.push_back(vector<char>(size));
+	}
+
+	void start(){
+
+	}
+};
+
+
 int main(){
 	return 0;
 }
