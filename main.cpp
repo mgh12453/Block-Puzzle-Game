12d59e4bd3adced2c3a22ee097d84e9f203b29b6
MA7887 <mohammadd2201048@gmail.com>
2023-01-22 15:20:51 +0330

    he he

--- main.cpp
+++ main.cpp
@@ -11,15 +11,47 @@ public:
 	game(int init_size = 11){
 		size = init_size;
 		for(int i = 0; i < size; i ++)
-			buff.push_back(vector<char>(size));
+			buff.push_back(vector<char>('-', size));
 	}
 
 	void start(){
+
+	}
+
+	void check (){
+
+	}
+
+	void move_left (){
+
+		check ();
+	}
+
+	void move_right (){
+		check ();
+	}
+
+	void move_down (){
+
+		check ();
+	}
+
+	void hint (){
+
+	}
+
+	void display (){
 
 	}
 };
 
 
 int main(){
+	game* g;
+	while (true) {
+		string type;
+		cin >> type;
+
+	}
 	return 0;
 }
